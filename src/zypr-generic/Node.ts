a792c276ef6d4e14600f4daf475a48dc12bb8c0a
Henry Blanchette <henry@monkeyface.com>
2022-11-16 09:35:56 +0000

    clean up formatWrapper

--- src/zypr-generic/Node.ts
+++ src/zypr-generic/Node.ts
@@ -9,11 +9,6 @@ export type Wrapper
     | { case: 'query-insert-top' } | { case: 'query-insert-bot' }
     | { case: 'query-invalid', string: string }
 
-// export type SimpleNodeCases =
-//     'cursor' |
-//     'select-top' | 'select-bot' |
-//     'query-replace' | 'query-insert-top' | 'query-insert-bot'
-
 export type ExpNode<Exp, Dat> = { exp: Exp, node: Node<Dat> }
 
 // uses last kid as the "real" exp, for the sake of ExpNode
@@ -24,41 +19,3 @@ export const formatWrapper =
             const { exp } = expNodes[expNodes.length - 1]
             return { exp, node: { case: 'wrapper', wrapper, kids: expNodes.map(expNode => expNode.node) } }
         }
-
-// export const formatCursorClaspAround =
-//     <Exp, Dat, Env>(kid: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => {
-//             const { exp, node } = kid(env)
-//             return { exp, node: { case: 'cursor', kids: [node] } }
-//         }
-
-// export const formatSelectClaspTopAround =
-//     <Exp, Dat, Env>(kid: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => {
-//             const { exp, node } = kid(env)
-//             return { exp, node: { case: 'select-top', kids: [node] } }
-//         }
-
-// export const formatSelectClaspBotAround =
-//     <Exp, Dat, Env>(kid: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => {
-//             const { exp, node } = kid(env)
-//             return { exp, node: { case: 'select-bot', kids: [node] } }
-//         }
-
-// export const formatQueryReplaceAround =
-//     <Exp, Dat, Env>(kidNew: (env: Env) => ExpNode<Exp, Dat>, kidOld: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => ({ case: 'query-replace', kids: [kidNew(env), kidOld(env)] })
-
-// export const formatQueryInsertTopAround =
-//     <Exp, Dat, Env>(kid: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => ({ case: 'query-insert-top', kids: [kid(env)] })
-
-
-// export const formatQueryInsertBotAround =
-//     <Exp, Dat, Env>(kid: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => ({ case: 'query-insert-bot', kids: [kid(env)] })
-
-// export const formatQueryInvalidAround =
-//     <Exp, Dat, Env>(string: string, kid: (env: Env) => ExpNode<Exp, Dat>) =>
-//         (env: Env): ExpNode<Exp, Dat> => ({ case: 'query-invalid', string, kids: [kid(env)] })
