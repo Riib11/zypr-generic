4d081b457a84005166b83391acf328887cdb39b9
Henry Blanchette <henry@monkeyface.com>
2022-12-08 00:06:17 -0500

    defined LanguageBeta, with lam and bnd

--- src/zypr-generic/language/LanguageBeta.ts
+++ src/zypr-generic/language/LanguageBeta.ts
@@ -1,66 +1,89 @@
-export const tmp = false
+import { List } from "immutable"
+import * as Language from "../Language"
 
-// import * as Language from "../Language"
-// export type Pre = Language.Pre<Met, Rul, Val>
-// export type Exp = Language.Exp<Met, Rul, Val>
-// export type Zip = Language.Zip<Met, Rul, Val>
+export type Pre = Language.Pre<Met, Rul, Val>
+export type Exp = Language.Exp<Met, Rul, Val>
+export type Zip = Language.Zip<Met, Rul, Val>
 
-// export type Met
-//     = 'bnd'
-//     | 'exp'
+export type Met
+  = 'bnd'
+  | 'exp'
 
-// export type Rul
-//     = 'bnd'  // bnd
-//     | 'var' | 'app' | 'lam' | 'hol' // exp
+export type Rul
+  // bnf
+  = 'bnd'
+  // exp
+  | 'var'
+  | 'app'
+  | 'lam'
+  | 'hol'
 
-// export type Val = BndVal | VarVal | AppVal | HolVal
-// export type BndVal = { label: string }
-// export type VarVal = { label: string }
-// export type AppVal = { indentedArg: boolean }
-// export type LamVal = { indentedBod: boolean }
-// export type HolVal = {}
+export type Val = BndVal | VarVal | AppVal | LamVal | HolVal
+export type BndVal = { label: string }
+export type VarVal = { label: string }
+export type AppVal = { indentedArg: boolean }
+export type LamVal = { indentedBod: boolean }
+export type HolVal = {}
 
-// export const isArg = (zip: Zip | undefined) =>
-//     zip !== undefined &&
-//     zip.rul === 'app' && zip.kidsLeft.size === 1
+export function isApl(zip: Zip | undefined): boolean {
+  return zip !== undefined && zip.rul === 'app' && zip.kidsLeft.size === 0
+}
 
-// export const isApl = (zip: Zip | undefined) =>
-//     zip !== undefined &&
-//     zip.rul === 'app' && zip.kidsLeft.size === 0
+export function isArg(zip: Zip | undefined): boolean {
+  return zip !== undefined && zip.rul === 'app' && zip.kidsLeft.size === 1
+}
 
-// export const isBod = (zip: Zip) =>
-//     zip !== undefined &&
-//     zip.rul === 'lam' && zip.kidsLeft.size == 1
+export function isBnd(zip: Zip | undefined): boolean {
+  return zip !== undefined && zip.rul === 'lam' && zip.kidsLeft.size === 0
+}
 
-// export default function grammar(): Language.Grammar<Met, Rul, Val> {
-//     return Language.buildGrammar<Met, Rul, Val>({
-//         rules: {
-//             'bnd': ['bnd'],
-//             'exp': ['var', 'app', 'lam', 'hol']
-//         },
-//         valueDefault: {
-//             // bnd
-//             'bnd': { label: "" },
-//             // exp
-//             'var': { label: "" },
-//             'app': { indentedArg: false },
-//             'lam': { indentedBod: false },
-//             'hol': {}
-//         },
-//         kids: {
-//             // bnd
-//             'bnd': [],
-//             // exp
-//             'var': [],
-//             'app': ['exp', 'exp'],
-//             'lam': ['bnd', 'exp'],
-//             'hol': []
-//         },
-//         holeRule: {
-//             'bnd': 'bnd',
-//             'exp': 'hol'
-//         },
-//         isParenthesized: undefined,
-//         isIndentable: undefined
-//     })
-// }
+export function isBod(zip: Zip | undefined): boolean {
+  return zip !== undefined && zip.rul === 'lam' && zip.kidsLeft.size === 1
+}
+
+export default function language(): Language.Language<Met, Rul, Val> {
+  let grammar: Language.Grammar<Met, Rul, Val> = {
+    rules: (met) => ({
+      'bnd': [] as Rul[],
+      'exp': ['var', 'app', 'hol'] as Rul[]
+    }[met]),
+    valueDefault: (rul) => ({
+      'bnd': { label: "" } as Val,
+      'var': { label: "" } as Val,
+      'app': { indentedArg: false } as Val,
+      'lam': { indentedBod: false } as Val,
+      'hol': {} as Val
+    }[rul]),
+    kids: (rul) => ({
+      'bnd': [] as Met[],
+      'var': [] as Met[],
+      'app': ['exp', 'exp'] as Met[],
+      'lam': ['bnd', 'exp'] as Met[],
+      'hol': [] as Met[]
+    }[rul]),
+    holeRule: (met) => ({
+      'bnd': 'bnd' as Rul,
+      'exp': 'hol' as Rul
+    }[met])
+  }
+
+  function isParenthesized(zips: List<Zip>, exp: Exp): boolean {
+    let zip = zips.get(0)
+    if (zip === undefined) return false
+    if (isArg(zip) && (exp.rul === 'app' || exp.rul === 'lam')) return true
+    return false
+  }
+
+  function isIndentable(zips: List<Zip>, exp: Exp): boolean {
+    let zip = zips.get(0)
+    if (zip === undefined) return false
+    if (isArg(zip) || isBod(zip)) return true
+    return false
+  }
+
+  return {
+    grammar,
+    isParenthesized,
+    isIndentable
+  }
+}
